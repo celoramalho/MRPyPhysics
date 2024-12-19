9fe859635a648c6a8e64f09627e043992a6120fa
Griffo <marceloramalho.info@gmail.com>
2024-12-18 22:22:33 -0300

    Correcting inheritance in MRPyPhysics class

--- mrpyphysics/mrpyphysics.py
+++ mrpyphysics/mrpyphysics.py
@@ -3,13 +3,14 @@ from .kinematics import Kinematics
 #from .dynamics import Dynamics
 #from .thermodynamics import Thermodynamics
 
-class MRPyPhysics:
+class MRPyPhysics(Unit, Kinematics):
     """
     Main class to provide access to all physics-related calculations.
     """
 
     def __init__(self):
-        self.kinematics = Kinematics()
-        self.unit = Unit()
+        pass
+        #self.kinematics = Kinematics()
+        #self.unit = Unit()
         #self.dynamics = Dynamics()
         #self.thermodynamics = Thermodynamics()
