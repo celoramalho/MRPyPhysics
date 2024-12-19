bbbe8991e59c1b8b762c7a30bee29042d9cefda4
Griffo <marceloramalho.info@gmail.com>
2024-12-18 22:54:04 -0300

    Adding more units and unit converting to isu

--- mrpyphysics/unit.py
+++ mrpyphysics/unit.py
@@ -1,27 +1,68 @@
 class Unit():
-    units = (
-    # Distance
-    "meters", "kilometers", "centimeters", "millimeters", "inches", "feet", "yards", "miles", "nautical miles",
+    valid_units = {
+        # Distance
+        "meters": "m",
+        "kilometers": "km",
+        "centimeters": "cm",
+        "millimeters": "mm",
+        "inches": "in",
+        "feet": "ft",
+        "yards": "yd",
+        "miles": "mi",
+        "nautical miles": "nmi",
 
-    # Time
-    "seconds", "milliseconds", "microseconds", "nanoseconds", "minutes", "hours", "days", "weeks", "months", "years",
+        # Time
+        "seconds": "s",
+        "milliseconds": "ms",
+        "microseconds": "µs",
+        "nanoseconds": "ns",
+        "minutes": "min",
+        "hours": "h",
+        "days": "d",
+        "weeks": "wk",
+        "months": "mo",
+        "years": "yr",
 
-    # Area
-    "square meters", "square kilometers", "square centimeters", "square millimeters", 
-    "hectares", "acres", "square inches", "square feet", "square yards", "square miles",
+        # Area
+        "square meters": "m²",
+        "square kilometers": "km²",
+        "square centimeters": "cm²",
+        "square millimeters": "mm²",
+        "hectares": "ha",
+        "acres": "ac",
+        "square inches": "in²",
+        "square feet": "ft²",
+        "square yards": "yd²",
+        "square miles": "mi²",
 
-    # Speed
-    "meters per second", "kilometers per hour", "miles per hour", 
-    "feet per second", "knots",
+        # Speed
+        "meters per second": "m/s",
+        "kilometers per hour": "km/h",
+        "miles per hour": "mph",
+        "feet per second": "ft/s",
+        "knots": "kn",
 
-    # Acceleration
-    "meters per second squared", "feet per second squared", "g-force",
+        # Acceleration
+        "meters per second squared": "m/s²",
+        "feet per second squared": "ft/s²",
+        "g-force": "g",
 
-    # Mass
-    "grams", "kilograms", "milligrams", "micrograms", "tonnes", 
-    "pounds", "ounces", "stone", "short tons", "long tons"
-)
+        # Mass
+        "grams": "g",
+        "kilograms": "kg",
+        "milligrams": "mg",
+        "micrograms": "µg",
+        "tonnes": "t",
+        "pounds": "lb",
+        "ounces": "oz",
+        "stone": "st",
+        "short tons": "st",
+        "long tons": "lt"
+    }
+
     def __init__(self, value, unit):
+        if unit not in self.valid_units or unit not in self.valid_units.values():
+            raise ValueError(f"Invalid unit: {unit}")
         self.value = value 
         self.unit = unit
     
